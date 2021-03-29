d505cfbe670e96389f76e983ee003cc2fe43695c
Aurelien Bacart <abacart@pm.me>
2021-03-29 21:08:25 +0200

    add lock

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket = "bacart.terraform.backend"
+    key    = "lab/remote.tfstate"
+    region = "eu-west-3"
+    dynamodb_table = "terraform-s3-state-lock"
+  }
+}
