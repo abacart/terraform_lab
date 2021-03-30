b46a3c4e1c88b5a9f4be64d347e3d69502abc0e7
Aurelien Bacart <abacart@pm.me>
2021-03-30 12:03:58 +0200

    Change SG on EC2

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "my_ec2" {
   ami           = "ami-0575fc648136871a1"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.allow_http.id]
+  vpc_security_group_ids = [aws_security_group.allow_https.id]
   tags = {
     Name = "Terrafrom_ec2_test"
   }
