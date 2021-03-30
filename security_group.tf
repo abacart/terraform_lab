3f6c2b3de458a096c22f7bf37fa79dd81c87e622
Aurelien Bacart <abacart@pm.me>
2021-03-30 11:55:03 +0200

    Change http to https

--- security_group.tf
+++ security_group.tf
@@ -1,12 +1,12 @@
 
-resource "aws_security_group" "allow_http" {
-  name        = "allow_http"
-  description = "Allow HTTP inbound traffic"
+resource "aws_security_group" "allow_https" {
+  name        = "allow_https"
+  description = "Allow HTTPs inbound traffic"
 
   ingress {
-    description = "HTTP from VPC"
-    from_port   = 80
-    to_port     = 80
+    description = "HTTPs from VPC"
+    from_port   = 443
+    to_port     = 443
     protocol    = "TCP"
     cidr_blocks = ["0.0.0.0/0"]
   }
