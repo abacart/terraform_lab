4fee7a42720d8bd9cbeac8ce71629cfd0d2a463a
Aurelien Bacart <abacart@pm.me>
2021-03-30 14:19:40 +0200

    Set backend to Terraform

--- backend.tf
+++ backend.tf
@@ -1,8 +1,10 @@
+/*
 terraform {
   backend "s3" {
-    bucket = "bacart.terraform.backend"
-    key    = "lab/remote.tfstate"
-    region = "eu-west-3"
+    bucket         = "bacart.terraform.backend"
+    key            = "lab/remote.tfstate"
+    region         = "eu-west-3"
     dynamodb_table = "terraform-s3-state-lock"
   }
 }
+*/
--- provider.tf
+++ provider.tf
@@ -1,4 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+  backend "remote" {
+    workspaces { name = "lab" }
+    hostname     = "app.terraform.io"
+    organization = "abacart"
+  }
+}
+
+
 # Configure the AWS Provider
 provider "aws" {
-  region     = "eu-west-3"
+  region = "eu-west-3"
 }
